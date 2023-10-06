977876e785b6156912af3f076c1001d343596d4f
Saul Lee <solleedata@gmail.com>
2023-10-06 16:39:12 +0900

    feat: More dialogue

---
+++ src/routes/more/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import MoreMenuPanel from '../../lib/components/MoreMenuPanel.svelte';
+	import LogoutConfirmDialog from '../../lib/components/LogoutConfirmDialog.svelte';
+	import AppLayout from '$lib/components/AppLayout.svelte';
+
+	let openLogoutConfirm = false;
+
+	const onReqLogout = () => {
+		openLogoutConfirm = true;
+	};
+</script>
+
+/** 더보기 */
+<AppLayout>
+	<div class="pt-[20px] px-[16px]">
+		<MoreMenuPanel on:reqlogout={onReqLogout} />
+	</div>
+
+	<LogoutConfirmDialog bind:open={openLogoutConfirm} />
+</AppLayout>
