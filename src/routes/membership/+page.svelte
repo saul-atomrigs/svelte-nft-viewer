27471b3323ed33e7cc2ff94b2725bb57504d51e8
Saul Lee <solleedata@gmail.com>
2023-10-06 16:34:23 +0900

    feat: Membership page

---
+++ src/routes/membership/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import AppLayout from '$lib/components/AppLayout.svelte';
+</script>
+
+<AppLayout>
+	<div class="w-full h-content flex justify-center items-center">
+		<div class="flex flex-col items-center text-center">
+			<img src="/images/speaker.png" width="150" alt="" />
+			<h1 class="sb-text-gray-900">멤버십 서비스는<br />준비중입니다.</h1>
+		</div>
+	</div>
+</AppLayout>
